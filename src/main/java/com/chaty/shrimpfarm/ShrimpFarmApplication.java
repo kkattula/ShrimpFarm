267186284a10c9ae45e2c3cd865c45e97bffc8a9
chaty <kbabachaitanya@gmail.com>
2017-08-20 22:57:04 -0400

    Initial Commit

---
+++ src/main/java/com/chaty/shrimpfarm/ShrimpFarmApplication.java
@@ -0,0 +1,12 @@
+package com.chaty.shrimpfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrimpFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrimpFarmApplication.class, args);
+	}
+}
---
+++ src/test/java/com/chaty/shrimpfarm/ShrimpFarmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chaty.shrimpfarm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShrimpFarmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
