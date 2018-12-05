f9a945fda99526443819a3ac71b3a872c8b18682
chaty <kbabachaitanya@gmail.com>
2018-12-05 08:46:39 -0500

    Added CORS Bean

--- src/main/java/com/chaty/shrimpfarm/ShrimpFarmApplication.java
+++ src/main/java/com/chaty/shrimpfarm/ShrimpFarmApplication.java
@@ -2,6 +2,10 @@ package com.chaty.shrimpfarm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ShrimpFarmApplication {
@@ -9,4 +13,14 @@ public class ShrimpFarmApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShrimpFarmApplication.class, args);
 	}
+	
+	@Bean
+	   public WebMvcConfigurer corsConfigurer() {
+	      return new WebMvcConfigurerAdapter() {
+	         @Override
+	         public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/farm/api/").allowedOrigins("http://aquajet.cfapps.io");
+	         }
+	      };
+	   }
 }
