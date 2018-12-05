637c90a0a6e17445fda3c045e9b39aacd29453ac
chaty <kbabachaitanya@gmail.com>
2018-12-05 09:42:09 -0500

    Added CORS to security config

---
+++ src/main/java/com/chaty/shrimpfarm/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.chaty.shrimpfarm;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://aquajet.cfapps.io"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry urlRed = http.cors().and()
+				.authorizeRequests().antMatchers("/farm/api/**").permitAll();
+
+		urlRed.anyRequest().permitAll();
+
+	}
+
+}
--- src/main/java/com/chaty/shrimpfarm/ShrimpFarmApplication.java
+++ src/main/java/com/chaty/shrimpfarm/ShrimpFarmApplication.java
@@ -2,31 +2,14 @@ package com.chaty.shrimpfarm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class ShrimpFarmApplication {
+public class ShrimpFarmApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShrimpFarmApplication.class, args);
 	}
-	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-	    return new WebMvcConfigurerAdapter() {
-	        @Override
-	        public void addCorsMappings(CorsRegistry registry) {
-	            registry.addMapping("/**")
-	                    .allowedOrigins("http://aquajet.cfapps.io")
-	                    .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-	                    .allowedHeaders("Content-Type", "authorization")
-	                    .exposedHeaders("Content-Type")
-	                    .maxAge(3600);
-	        }
-	    };
-	}
+
 	
 }
