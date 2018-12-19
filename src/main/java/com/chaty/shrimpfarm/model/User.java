acb972bc3e3f6bc62e22880f88da6a2d639fd36a
chaty <kbabachaitanya@gmail.com>
2018-12-19 14:52:01 -0500

    Mongo User Authentication Added

---
+++ src/main/java/com/chaty/shrimpfarm/model/User.java
@@ -0,0 +1,35 @@
+package com.chaty.shrimpfarm.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "user")
+public class User {
+	@Id
+	public String _id;
+
+	public String username;
+	
+	public String token;
+	
+	public LocalDate dateCreated;
+	
+	public String email;
+	
+	public String image;
+
+	public String nickname;
+
+	public String status;
+
+	public List<String> roles;
+	
+	public List<Farm> farm;
+
+}
